3d9982329ff57987d420eaa070d64aa11e5a3790
Anthony15224 <Anthony15224@hotmail.com>
2020-02-18 17:22:21 -0600

    !

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 
-var TaskSchema = new Schema({a
+var TaskSchema = new Schema({
   taskItem: {
     type: String,
     required: true
