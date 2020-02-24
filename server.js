655417c6dbd12c49d6e0420c9754e3afccaff43b
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-24 13:51:59 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,24 +3,60 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 // const bcrypt = require('bcrypt')
-
-
 const passport = require('./config/passport');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// app.set('view-engine', 'ejs')
+// app.use(express.urlencoded({ extended: false }))
+// const users = [];
+
+// //passport
+// app.get('/', (req, res) => {
+//   res.render('index.ejs', { name: 'bob' })
+// })
+
+// app.get('/login', (req, res) => {
+//   res.render('login.ejs')
+// })
+// app.post('/login', (req, res) => {
+  
+// })
+
+// app.get('/register', (req, res) => {
+//   res.render('register.ejs')
+// })
+
+// app.post('/register', async (req, res) => {
+//   try {
+//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//     users.push({
+//       id: Date.now().toString(),
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: hashedPassword
+//     })
+//     res.redirect('/login')
+//   } catch {
+//     res.redirect('/register')
+//   }
+//   console.log(users)
+// })
+// Define middleware here
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
+
  
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+require('./routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (_, res) => {
@@ -28,7 +64,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-require('./routes')(app);
 
 app.listen(PORT, function() {
   console.log(
@@ -37,4 +72,3 @@ app.listen(PORT, function() {
     PORT
   );
 });
-// module.exports = app;
