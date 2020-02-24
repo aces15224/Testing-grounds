eb75930cd18c0b72ee03bd9ffaa2743725215798
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-24 13:55:52 -0600

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
-// import Wrapper from "./components/Wrapper";
-// import TaskBank from "./components/TaskBank";
-// import TaskList from "./components/TaskList";
 import CalendarPage from "./pages/calendar";
-// import Dashboard from "./pages/dashboard";
 import taskBank from "./pages/TaskBankHome";
 import PomodoroTimer from "./pages/pomodoroTimer";
 import UserStats from "./pages/userStats";
+import logo from "./checkMark.jpg"
 import './App.css';
 
 class App extends Component {
@@ -16,16 +13,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <NavBar />
-            
-            <Route exact path="/" component={CalendarPage} />
-            {/* <Route exact path="/dashboard" component={Dashboard} /> */}
-            <Route exact path="/tasks" component={taskBank} />
-            <Route exact path="/timer" component={PomodoroTimer} />
-            <Route exact path="/stats" component={UserStats} />
-
-          
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <NavBar image={logo}/>
+            </div>
+            <div className="col-8" id="mainDisplay">
+              <Route exact path="/" component={CalendarPage} />
+              <Route exact path="/tasks" component={taskBank} />
+              <Route exact path="/timer" component={PomodoroTimer} />
+              <Route exact path="/stats" component={UserStats} />
+            </div> 
+          </div>     
         </div>
       </Router>
     )
