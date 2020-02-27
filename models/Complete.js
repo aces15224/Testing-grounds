801cf9fc9188591e07b9b44eadfdb7f1240fe6f9
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-26 20:20:15 -0600

    Add files via upload

--- models/Complete.js
+++ models/Complete.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 var CompleteSchema = new Schema({
   category: {
     type: String, 
-    enum: ['Career', 'Education', 'Fitness', 'Personal', 'Health', 'Chores'], 
   }
 
 });
