a80f4a25e8e691e0b56a11e690b32c80cb2dbf4a
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-26 19:48:05 -0600

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds237267.mlab.com:37267/heroku_0n06ck2s";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds225375.mlab.com:25375/heroku_5b0wf0xm";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
@@ -36,6 +36,21 @@ module.exports = function (app) {
       } else {
         return res.send(true)
       }
+    });
+  });
+
+  app.post("/api/completed", function(req, res) {
+    db.Complete.create({
+      category: req.body.category,
+    }).then(function(dbModel) {
+      console.log("yes!")
+      res.json(dbModel);
+    });
+  });
+  
+  app.get("/api/completed", function(req, res) {
+    db.Complete.find({}).then(function(dbModel) {
+      res.json(dbModel);
     });
   });
 
